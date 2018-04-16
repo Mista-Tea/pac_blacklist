491daeba3d9b5d7b6debbff8459288e18f10b9fc
Mista-Tea <thatguythomas@hotmail.com>
2018-04-15 18:47:06 -0600

    Barebones setup

---
+++ lua/autorun/cl_pac_blacklist.lua
@@ -0,0 +1,18 @@
+-- Add user to blacklist, clear parts, request outfits from server
+concommand.Add( "pac_blacklist_add", function( ply, cmd, args, str )
+
+end )
+
+-- Remove user from blacklist, request outfits from server
+concommand.Add( "pac_blacklist_remove", function( ply, cmd, args, str )
+
+end )
+
+-- Clear the blacklist, request outfits from server
+concommand.Add( "pac_blacklist_clear", function( ply, cmd, args, str )
+
+end )
+
+hook.Add( "pace_WearPartFromServer", "BlockPACs", function( owner, part_data, data )
+	
+end )
